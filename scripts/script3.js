f5e636e5ac769ab354db3dc94838ea54a8aeebd7
Dobre Aurel-Ninel <101126905+AurelDobre20@users.noreply.github.com>
2023-01-07 17:57:21 +0200

    Delete some comments

--- scripts/script3.js
+++ scripts/script3.js
@@ -178,9 +178,9 @@ map.on('click', async function(e) {
         iconUrl: checkInterval(noHive),
         
     
-        iconSize:     [38, 95], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        iconSize:     [38, 95], 
+        iconAnchor:   [22, 94], 
+        popupAnchor:  [-3, -76] 
     });
 
 
